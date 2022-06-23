58e37ac793762ebdc1740e75b3dd6f45d2a21e91
STRsplit <steve.t.reed@gmail.com>
2022-06-23 15:06:23 -0500

    revised

---
+++ lib/utils/merkletree.d.ts
@@ -0,0 +1,12 @@
+import MerkleTreeJS from "merkletreejs";
+/**
+ * Simple wrapper over the MerkleTree in merkletreejs.
+ * Handles hashing identifiers to be compatible with Seaport.
+ */
+export declare class MerkleTree {
+    tree: MerkleTreeJS;
+    constructor(identifiers: string[]);
+    getProof(identifier: string): string[];
+    getRoot(): string;
+    getRootNumber(): string;
+}
